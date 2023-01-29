2ebb53e7b0448a45e9c94d26b6d3201e651f25b3
nikkolasg <nikkolasg@gmail.com>
2023-01-29 17:24:00 +0100

    opening fp module

--- src/lib.rs
+++ src/lib.rs
@@ -38,7 +38,11 @@ mod tests;
 
 // export for benchmarking only
 #[cfg(feature = "__private_bench")]
-pub use crate::{fp::Fp, fp12::Fp12, fp2::Fp2};
+pub use crate::{
+    fp::{self, Fp},
+    fp12::Fp12,
+    fp2::Fp2,
+};
 
 use ff::Field;
 use group::prime::PrimeCurveAffine;
