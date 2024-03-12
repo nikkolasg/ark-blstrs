6adb8f0146686e3559d17a3bef8c6870a9225eb0
Nicolas Gailly <nikkolasg@users.noreply.github.com>
2024-03-12 13:11:29 +0100

    Update fp.rs

--- src/fp.rs
+++ src/fp.rs
@@ -1,6 +1,7 @@
 //! This module provides an implementation of the BLS12-381 base field `GF(p)`
 //! where `p = 0x1a0111ea397fe69a4b1ba7b6434bacd764774b84f38512bf6730d2a0f6b0f6241eabfffeb153ffffb9feffffffffaaab`
-//! and this is a new commit
+//! and this is a old commit that is being renewed
+//! hello
 use blst::*;
 
 use core::{
